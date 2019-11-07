497c186e83d3ff6abb641584b6551d8a77dd86b5
Mummidi <dhanushkumarmummidi.ca@gmail.com>
2019-11-07 15:51:04 -0500

    new sample

---
+++ sample.php
@@ -0,0 +1,24 @@
+<?php
+// myvar = 0;
+// $myvar = 0;
+// $var5 = 0;
+// $_myvar = 0;
+// $_5var = 0;
+// $__element1 = 0;
+// $age* = 0;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Variables identifiers</title>
+</head>
+<body>
+    <header>
+        <h1>Variables identifiers</h1>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
